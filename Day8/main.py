c6555b3ff62729feaed457b0016279f5a238c8f4
Chris Castellano <c.castellano18@gmail.com>
2022-11-06 22:19:12 -0600

    day8

--- Day8/main.py
+++ Day8/main.py
@@ -1,11 +1,9 @@
 from operator import indexOf
+from tkinter.messagebox import YES
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 # def encrypt(plain_text, shift_amount):
 #   cypher_text = ''
@@ -25,13 +23,29 @@ def caesar(start_text, shift_amount, cipher_direction):
   end_text = ''
   if cipher_direction == "decode":
     shift_amount *= -1
-  for letter in start_text:
-    position = alphabet.index(letter)
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
     
-    new_position = position + shift_amount
-    end_text += alphabet[new_position] 
-  
+      new_position = position + shift_amount
+      end_text += alphabet[new_position] 
+    else:
+      end_text += char
   print(f"The {cipher_direction}d text is {end_text}")
+  
 
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+play = True
+while play:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  
+  result = input("Type 'yes' to continue playing and 'no' to stop?\n")
+  if result == "no":
+    play = False
+
