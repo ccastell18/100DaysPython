17c103c8aafd7c3f296b0d327a61daf169b9eebf
Chris Castellano <c.castellano18@gmail.com>
2022-11-06 19:37:12 -0600

    day 7

--- Day7/main.py
+++ Day7/main.py
@@ -1,85 +1,39 @@
 import random
+import hangman_art as ha
+import hangman_words as hw
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-lives = 6
-
-word_list = ["aardvark", "baboon", "camel"]
-
-chosen_word = random.choice(word_list)
-print(chosen_word)
-
-display = []
+chosen_word = random.choice(hw.word_list)
 word_length = len(chosen_word)
-for _ in range(word_length):
-  display += '_'
-print(display)
 
 end_of_game = False
+lives = 6
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+
+for _ in range(word_length):
+    display += "_"
 
 while not end_of_game:
-  guess  = input("Choose a letter? ").lower()
-  for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-      display[position] = letter
-  print(f"{' '.join(display)}")
+    guess = input("Guess a letter: ").lower()
 
-  if "-" not in display:
-    end_of_game = True
-    print("you win!!")
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(ha.stages[lives])
