5974d32b38a5eae0e21abebeea8b01224a7d4ac1
Chris Castellano <c.castellano18@gmail.com>
2024-03-26 21:58:27 -0500

    day7

--- Day7/main.py
+++ Day7/main.py
@@ -1,12 +1,11 @@
 #Step 1 
+from art import logo, stages
+from words import word_list as wl
 import random
-word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+word_list = wl 
+end_of_game = False
+lives = 6
 display = []
 chosen_word = random.choice(word_list)
 
@@ -14,32 +13,31 @@ for char in chosen_word:
   display.append("_")
 print(display)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
-end_of_game = False
-
+print(logo)
 while not end_of_game:
+  
   guess = input("Guess a letter? ").lower()
-
-#for letter in chosen_word:
- # if letter == guess:
-  #  chosen_word[letter] = guess
-  #else:
-   #chosen_word[letter] = chosen_word[letter]
 
   for position in range(len(chosen_word)):
     letter = chosen_word[position]
+    
     if letter == guess:
       display[position] = letter
+
   print(display)
+
+  if guess not in chosen_word:
+    lives -= 1
+    if lives == 0:
+      end_of_game == True
+      print("you lose")
+
+
+
   if "_" not in display:
     end_of_game == True
+
+  print(stages[lives])
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
