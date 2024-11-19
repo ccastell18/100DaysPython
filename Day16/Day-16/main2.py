054e328be6afa487b919c719f709836255a84469
Chris Castellano <c.castellano18@gmail.com>
2024-11-18 20:42:09 -0600

    Day 16

---
+++ Day16/Day-16/main2.py
@@ -0,0 +1,28 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# timmy.shape('turtle')
+# timmy.color('red')
+# timmy.forward(100)
+
+# my_screen = Screen()
+# my_screen.exitonclick()
+# timmy = Turtle()
+# timmy.shape('turtle')
+# timmy.color('red')
+# timmy.forward(100)
+
+# my_screen = Screen()
+# my_screen.exitonclick()
+
+
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+# table.add_column('Pokemon Name', ['Pikachu','Squirtle', 'Charmander'])
+# table.add_column('Type', ['Electric', 'Water', 'Fire'])
+
+
+# print(table)
+
+
