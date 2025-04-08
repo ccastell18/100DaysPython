bcdd2810f7d6bb23d5d10236e0a3a8aa40a29988
Chris Castellano <c.castellano18@gmail.com>
2025-04-07 20:57:41 -0500

    Day23 A

---
+++ Day23/main.py
@@ -0,0 +1,25 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
+
+player = Player()
+screen.onkey(player.go_up, "Up")
+
+car_manager = CarManager()
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
